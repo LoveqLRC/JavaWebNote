bea296f4d3b8454a71ed4c57310c1e637848e960
liao <664215432@qq.com>
2018-01-06 22:46:52 +0800

    Initial commit

---
+++ src/main/java/com/loveqrc/note/HelloServlet.java
@@ -0,0 +1,10 @@
+package com.loveqrc.note;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(
+        urlPatterns = "/hello"
+)
+public class HelloServlet extends HttpServlet {
+}
---
+++ src/main/java/com/loveqrc/note/download/DownLoadServlet.java
@@ -0,0 +1,46 @@
+package com.loveqrc.note.download;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+
+@WebServlet(
+        name = "download",
+        urlPatterns = "/download"
+)
+public class DownLoadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String fileName = req.getParameter("name");
+
+//        fileName = new String(fileName.getBytes("iso8859-1"), "utf-8");
+        ServletContext context = getServletContext();
+        String fileMimeType = context.getMimeType(fileName);
+
+        resp.setContentType(fileMimeType);
+
+        resp.setHeader("content-disposition",
+                "attachment;filename=" + new String(fileName.getBytes("utf-8"), "ISO-8859-1"));
+        InputStream is = context.getResourceAsStream("/download/" + fileName);
+        ServletOutputStream outputStream = resp.getOutputStream();
+
+        IOUtils.copy(is, outputStream);
+
+        is.close();
+        outputStream.close();
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
